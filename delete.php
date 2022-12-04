7844c4fca9c22472e02a740732882e2a6f2e3bb5
Mahan Choudhury <mahanc2210@gmail.com>
2022-12-04 14:26:09 +0600

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,25 @@
+<?php 
+// ob_start();
+include "config.php";
+
+
+
+if(isset($_GET['delete'])){
+
+    $s_no = $_GET['s_no_delete'];
+    $sql= "DELETE FROM info WHERE s_no='$s_no'";
+    $result = mysqli_query($conn, $sql);
+    
+    if($result){ 
+        
+        echo '<script type="text/javascript"> alert("Data Deleted") </script>';
+        header("location:datashow.php");
+    }
+    else{
+        echo "The record was not deleted successfully because of this error ---> ". mysqli_error($conn);
+    } 
+
+
+}
+// ob_end_flush();
+?>
